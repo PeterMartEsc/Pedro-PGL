f2eee471863ad2077436d2ba9667a17293fc2a05
PeterMartEsc <petermartesc@gmail.com>
2024-09-17 15:26:55 +0100

    refactor: Se sigue trabajando en el codigo del memory en clase

--- practicasJavaScript/memory/memory.js
+++ practicasJavaScript/memory/memory.js
@@ -1,43 +1,28 @@
 const DOM = {
     contenedorSup : document.getElementById("contenedorSuperior")
+    //body: document.querySelector("body"),
+    //Cuidado con querySelectorAll, por que devuelve un array con los elementos contenidos dentro
 
 }
 
-function crearNumerosCasillas(){
+let contenedor = document.createElement("div");
+contenedor.id="casilla";
+DOM.contenedorSup.appendChild(contenedor);
+
+
+function crearCasillas(){
     let maximo = 9;
-    let numero;
-
     DOM.contenedorSup.innerHTML="";
-
     
     for(let i = 1 ; i <= maximo; i++ ){
     
         let contenedor = document.createElement("div")
         contenedor.className = "contenedor"+i;
 
-        contenedor.innerHTML = ""+Math.round(1,9);
+        //contenedor.innerHTML = ""+Math.round(1,9);
         DOM.contenedorSup.appendChild(contenedor);
     }
 
 }
 
-function generarNumeros(){
-    let hecho = 0;
-    let comprobador = new int[8];
-
-    do{
-
-        let numRandom = Math.round(1,9);
-        for(let i = 0 ; i <=8;i++){
-            let veces = 0;
-            if(comprobador[i]==numRandom){
-                veces++
-            }
-        }
-
-
-    } while (hecho == 0);
-}
-
-crearNumerosCasillas();
 
