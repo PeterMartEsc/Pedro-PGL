bb5498949aaebcda8044e150ce3b69ca0d35751f
PeterMartEsc <petermartesc@gmail.com>
2024-09-19 13:09:47 +0100

    refactor: Sigo trabajando en el memory
    Ya puedo generar los numeros random sin que se repitan, me falta solo poder duplicar el array, generar un random más, meter todo eso en un nuevo array, mezclarlo y que se distrubuyan en las casillas

--- practicasJavaScript/memory/memory.js
+++ practicasJavaScript/memory/memory.js
@@ -10,37 +10,23 @@ const DOM = {
 
 function crearCasillas(){
     let maximo = 9;
+    let arrSet = new Set();
     DOM.contenedorSup.innerHTML="";
+
+    for(let i = 0; i<4;i++){
+        arrSet.add( Math.floor( Math.random(1,9) ) );
+    }
+
+    let arrayNoSet = Array.from(arrSet);
+
 
     for(let i = 1 ; i <= maximo; i++ ){
 
         let casilla = document.createElement("div");
         casilla.id="casilla"+$i;
         DOM.contenedorSup.appendChild(casilla);
-
-        crearNumeroRandom();
     }
 
 }
 
-function crearNumeroRandom(){
-    let veces=1;
-
-    let arr = new int[maximo];
-
-    for(let i = 1; i<Math.trunc(maximo/2);i++){
-        
-
-        
-    }
-
-}
-
-
-function crearNumeroRandom2(){
-    let maximo = 9;
-    let arrSet = new Set();
-
-    
-}
 
