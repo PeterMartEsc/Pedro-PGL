886e87e2179a55f175d4e6fcfaf1ac3e27f6a1b0
PeterMartEsc <petermartesc@gmail.com>
2024-10-03 11:48:49 +0100

    feat: Se trabajan la practic  1 y 2

--- 1erTrimestre/React/practicas-grandes/pr02/pr02-app/src/index.js
+++ 1erTrimestre/React/practicas-grandes/pr02/pr02-app/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';// también puede aparecer como: ‘react-dom/client’
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const mensaje = <h1>Vamos a renderizar este mensaje en nuestra web</h1>;
+const divRoot = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render( mensaje, divRoot);
