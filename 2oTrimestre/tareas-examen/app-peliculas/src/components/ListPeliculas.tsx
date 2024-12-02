184c00cb2ab63371036250c8d7d40e7da0a2e42c
PeterMartEsc <petermartesc@gmail.com>
2024-12-02 22:21:55 +0000

    feat(app-peliculas): Se logran obtener los datos de la api

--- 2oTrimestre/tareas-examen/app-peliculas/src/components/ListPeliculas.tsx
+++ 2oTrimestre/tareas-examen/app-peliculas/src/components/ListPeliculas.tsx
@@ -5,27 +5,29 @@ import Pelicula from '../models/Pelicula';
 
 type Props = {}
 
-interface Peliculas {
+/*interface Peliculas {
     results : Pelicula[];
-}
+}*/
 
 const ListPeliculas = (props: Props) => {
     
     const [peliculas, setPeliculas] = useState<Pelicula[]>([]);
-    const uri : string  = "http://localhost:3000/";
+    const uri : string  = "http://localhost:3000/peliculas";
 
     useEffect(() => {
         async function getPelicula(direccion : string){
             const response = await axios.get(direccion);
-            let listaPeliculas = response.data as Peliculas;
-            setPeliculas(listaPeliculas.results);
+            let listaPeliculas = response.data as Pelicula[];
+            setPeliculas(listaPeliculas);
         }
 
         getPelicula(uri);
     }, [])
 
+    console.log(peliculas);
+
     return (
-        <div></div>
+        <div>{JSON.stringify(peliculas)}</div>
     )
 }
 
