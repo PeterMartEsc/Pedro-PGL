06c26bafba2260ab07c5c98738fb99e672b888b6
PeterMartEsc <petermartesc@gmail.com>
2025-01-09 20:04:07 +0000

    feat(pr23 react native): Se continua la pr23

--- 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr23/navigation/PrincipalScreenPr23.tsx
+++ 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr23/navigation/PrincipalScreenPr23.tsx
@@ -1,22 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/Home';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { NativeStackScreenProps, createNativeStackNavigator } from '@react-navigation/native-stack';
 import EditNote from '../screens/EditNote';
-import { AppContextProvider } from '../components/AppContextProvider';
+import { AppContext, AppContextProvider } from '../components/AppContextProvider';
+import Nota from '../models/Nota';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type Props = {}
 
 type PrincipalStackParamList = {
-    Home: {title: string},
-    EditNote: undefined,
+    Home: undefined,
+    EditNote: {idNota: number},
 }
 
 const Stack = createNativeStackNavigator<PrincipalStackParamList>();
 
 const PrincipalScreenPr23 = (props: Props) => {
 
-    const [title, settitle] = useState("");
+    const [title, settitle] = useState("Tareas");
 
     return (
         <AppContextProvider>
@@ -34,4 +35,58 @@ const PrincipalScreenPr23 = (props: Props) => {
 
 export default PrincipalScreenPr23
 
-const styles = StyleSheet.create({})
+type PropsHome = NativeStackScreenProps<PrincipalStackParamList, 'Home'>
+
+function Home({navigation, route}:PropsHome){
+    
+    const {listaNotas, setListaNotas} = useContext(AppContext);
+    const [contadorIds, setcontadorIds] = useState(0);
+
+    function addItem(){
+        let id = contadorIds;
+        let aux = new Nota(id, "New Note", "", false);
+        setListaNotas([...listaNotas, aux]);
+        setcontadorIds(id+1);
+    }
+
+    function deleteNote(index : number){
+        let aux = [];
+        for ( let i = 0; i < listaNotas.length; i++ ) {
+            if(i != index){
+                aux.push(listaNotas[i]);
+            }
+        }
+        setListaNotas(aux);
+    }
+
+    return (
+        <View style={{flex:1}}>
+            <FlatList
+                data={listaNotas}
+                renderItem={({item, index}) => {
+                    return(
+                        <View /*style={index%2 == 0 ? styles.pares : styles.impares}*/>
+                            <Text style={{justifyContent: 'center'}}>
+                                <Icon size={30} onPress={()=>{item.acabada=!item.acabada; setListaNotas([...listaNotas])}} name={item.acabada ? 'checkbox-outline' : 'square-outline' }/>
+                                {item.nombre}
+                                <Icon size={30} onPress={()=>navigation.navigate('EditNote', {idNota:item.id, title: 'Detail Page'})} name='pencil-outline'/>
+                                <Icon size={30} onPress={()=>deleteNote(index)} name='trash'/>
+                            </Text>
+                        </View>
+                    )
+                }}
+                keyExtractor={(item, index) => item.nombre + index}
+                ListHeaderComponent={()=> <Text></Text>}
+                ItemSeparatorComponent={()=> <Text>**************************************</Text>}
+            />
+            <View style={{backgroundColor: 'blue', height: 30, width: 200, justifyContent: 'center', margin: 'auto', borderRadius: 5}}>
+                <TouchableOpacity onPress={addItem}>
+                    <Text>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+})
--- 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr23/screens/EditNote.tsx
+++ 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr23/screens/EditNote.tsx
@@ -1,15 +1,53 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Switch, Text, TextInput, View } from 'react-native'
 import React, { useContext } from 'react'
 import { AppContext } from '../components/AppContextProvider'
+import Nota from '../models/Nota'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 type Props = {}
 
-const EditNote = (props: Props) => {
+type PrincipalStackParamList = {
+    Home: {title: string},
+    EditNote: {idNota: number},
+}
 
+type PropsEditNote = NativeStackScreenProps<PrincipalStackParamList, 'EditNote'>
+function EditNote ({navigation, route}: PropsEditNote) {
+
+    const {listaNotas, setListaNotas} = useContext(AppContext);
+
+    function fillFormData(value : string, field: keyof Nota){
+
+        let id = route.params.idNota;
+
+        switch(field){
+            case 'nombre':
+                listaNotas[id].nombre = value;
+                break;
+            case 'contenido':
+                listaNotas[id].contenido = value;
+                break;
+        }
+
+        setListaNotas([...listaNotas]);
+    }
+
+    function alternarAcabada(){
+        let id = route.params.idNota;
+        listaNotas[id].acabada =! listaNotas[id].acabada;
+        setListaNotas([...listaNotas]);
+    }
 
     return (
         <View>
-            <Text>Hola</Text>
+            <TextInput placeholder='nombre' onChangeText={(texto) => fillFormData(texto, "nombre")} defaultValue={listaNotas[route.params.idNota].nombre}/>
+            <TextInput placeholder='contenido' onChangeText={(texto) => fillFormData(texto, "contenido")} defaultValue={listaNotas[route.params.idNota].contenido}/>
+            <Switch
+                trackColor={{false: '#66d1c9', true: '#d16672'}}
+                thumbColor={listaNotas[route.params.idNota].acabada ? '#66d1c9' : '#d16672'}
+                onValueChange={alternarAcabada}
+                value={listaNotas[route.params.idNota].acabada}
+            />
         </View>
     )
 }
