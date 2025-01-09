a516ef8d6eebd4f173e0a07744de6d518f4539ac
PeterMartEsc <petermartesc@gmail.com>
2025-01-09 20:29:43 +0000

    refactor: Cambios menores

--- 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr24/navigation/PrincipalNavigationPr24.tsx
+++ 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr24/navigation/PrincipalNavigationPr24.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 type Props = {}
 
