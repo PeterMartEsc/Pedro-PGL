582fb1aff293223bf1cf8188f766b7920bfac65e
PeterMartEsc <petermartesc@gmail.com>
2025-01-14 13:20:36 +0000

    feat(pr25): Se realiza la pr25

---
+++ 2oTrimestre/react-native/dossier/dossierproject/practicas/2-navigation/pr25/screens/Pantalla2Screen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Pantalla2Screen = (props: Props) => {
+  return (
+    <View>
+      <Text>Pantalla2Screen</Text>
+    </View>
+  )
+}
+
+export default Pantalla2Screen
+
+const styles = StyleSheet.create({})
