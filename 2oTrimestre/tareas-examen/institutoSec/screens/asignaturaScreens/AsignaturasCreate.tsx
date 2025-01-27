6cb59ecaa25c974a4e9d4014062f0a36c78e5a1a
PeterMartEsc <petermartesc@gmail.com>
2025-01-27 11:27:38 +0000

    feat(institutoSec): Se realiza la navegación principal de la aplicación de instituto que consume la api securizada

---
+++ 2oTrimestre/tareas-examen/institutoSec/screens/asignaturaScreens/AsignaturasCreate.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const AsignaturasCreate = (props: Props) => {
+  return (
+    <View>
+      <Text>AsignaturasCreate</Text>
+    </View>
+  )
+}
+
+export default AsignaturasCreate
+
+const styles = StyleSheet.create({})
---
+++ 2oTrimestre/tareas-examen/institutoSec/screens/matriculaScreens/MatriculasListPrincipal.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const MatriculasListPrincipal = (props: Props) => {
+  return (
+    <View>
+      <Text>MatriculasListPrincipal</Text>
+    </View>
+  )
+}
+
+export default MatriculasListPrincipal
+
+const styles = StyleSheet.create({})
